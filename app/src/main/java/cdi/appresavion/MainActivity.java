18a3c200c961ba3f9ff0109d5e2faf80dc7eb52d
bigwanjeog <wenaud@live.fr>
2016-09-02 11:36:08 +0200

    .

--- app/src/main/java/cdi/appresavion/MainActivity.java
+++ app/src/main/java/cdi/appresavion/MainActivity.java
@@ -5,10 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        i = 5;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
