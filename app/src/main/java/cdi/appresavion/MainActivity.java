0b08eebaae35a5e7003f3b3919aa0d5b733d0a33
Kr0uK <boris.motz@gmail.com>
2016-09-02 11:12:21 +0200

    .

--- app/src/main/java/cdi/appresavion/MainActivity.java
+++ app/src/main/java/cdi/appresavion/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    int i;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
