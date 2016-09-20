e26a5dcf97e4b3e10253b4cb6c718c70947eb658
Kr0uK <boris.motz@gmail.com>
2016-09-20 08:55:02 +0200

    Acitivity Détails faite (design) // TODO  : code de l'activité

--- app/src/main/java/cdi/appresavion/DetailsActivity.java
+++ app/src/main/java/cdi/appresavion/DetailsActivity.java
@@ -7,15 +7,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class NouvelUserActivity extends AppCompatActivity {
+public class DetailsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nouvel_user);
-// TODO inscription nouvel user
+        setContentView(R.layout.activity_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-
+// TODO code
     }
 
 }
