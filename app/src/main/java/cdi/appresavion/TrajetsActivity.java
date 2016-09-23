fbbacbbb72919366f4f8ea367f6204bf8f2119ba
NeoRyu <coupez.frederic@gmail.com>
2016-09-23 10:50:10 +0200

    ♫ J'aime les licornes et leur jolie corne... ♫

--- app/src/main/java/cdi/appresavion/TrajetsActivity.java
+++ app/src/main/java/cdi/appresavion/TrajetsActivity.java
@@ -32,6 +32,8 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
+// <!-- ♫ J'aime les licornes et leur jolie corne... ♫ -->
 /*
  * Affichage des trajets disponibles
  * TODO : -> prendre en compte que si l'on ne viens pas de RechercheActivity (ou que l'envoi est vide), il n'y ai pas de bugs !
